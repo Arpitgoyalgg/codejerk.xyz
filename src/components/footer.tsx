b6d46ae596730ae9b05508c92ad9f15819047c88
Arpit Goyal <50814175+Arpitgoyalgg@users.noreply.github.com>
2022-07-22 16:08:40 +0530

    Fix: copyright

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -37,7 +37,7 @@ export function Footer() {
               className="self-center text-xs tracking-wider md:self-end"
               aria-label="Copyright"
             >
-              © 2020 - 2021 Arpit Goyal. All Rights Reserved.
+              © 2021 - 2022 Arpit Goyal. All Rights Reserved.
             </span>
             <div className="flex flex-row items-center justify-center space-x-6 md:justify-end">
               <span>

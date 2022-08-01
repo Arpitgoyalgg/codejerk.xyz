f45554658d0cab60d84e18e737bc0195078c01d1
Arpit Goyal <50814175+Arpitgoyalgg@users.noreply.github.com>
2022-08-01 14:21:49 +0530

    Abstract DinoCount as a component

---
+++ src/components/DinoCount.js
@@ -0,0 +1,84 @@
+import { useEffect, useState, useRef } from 'react'
+import Image from 'next/image'
+
+export function DinoCount(props) {
+  const [existingBinData, setExistingBinData] = useState()
+  const [count, setCount] = useState()
+  const [geoData, setGeoData] = useState()
+  const isMounted = useRef(false)
+
+  useEffect(() => {
+    fetch(`https://api.jsonbin.io/v3/b/62d97a21248d43754ffe585b/latest`)
+      .then(response => response.json())
+      .then(binData => {
+        setExistingBinData(binData.record)
+        setCount(binData.record.count)
+        console.log(binData)
+      })
+
+    fetch(`https://geolocation-db.com/json/`)
+      .then(response => response.json())
+      .then(geoData => setGeoData(geoData))
+  }, [])
+
+  useEffect(() => {
+    if (isMounted.current) {
+      console.log('Previous Data below')
+      console.log(existingBinData)
+      let prevGeo = existingBinData.geoData
+      prevGeo.push(geoData)
+      let newData = {
+        ...existingBinData,
+        count: count,
+        geoData: prevGeo,
+      }
+
+      const requestOptions = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newData),
+      }
+      fetch(
+        'https://api.jsonbin.io/v3/b/62d97a21248d43754ffe585b',
+        requestOptions
+      )
+        .then(response => response.json())
+        .then(data => console.log('Getting sneaky, huh?'))
+    } else {
+      isMounted.current = true
+    }
+  }, [count])
+
+  function handleCountCountry() {
+    setCount(count + 1)
+  }
+
+  return (
+    <div>
+      <div className="flex items-center justify-center w-full h-full">
+        <div className="w-32 h-32 overflow-hidden rounded-lg md:h-40 md:w-40 lg:h-48 lg:w-48">
+          <Image
+            src="/images/giphy.webp"
+            alt="My avatar"
+            width={256}
+            height={256}
+            quality={100}
+            priority={true}
+          />
+        </div>
+      </div>
+      <div className="mt-2 ml-2">
+        Here's someone greeting.{' '}
+        <span className="text-pink-600">
+          <b>Cute, right?</b>
+        </span>
+        <div
+          className="text-center mt-2 animate-bounce"
+          onClick={handleCountCountry}
+        >
+          💖 {count}
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,55 +9,9 @@ import Quote from '@/components/quote'
 import { CustomArrowDown } from '@/components/atoms'
 import { Contact } from '@/components/contact'
 import { getAllFrontMatters } from '@/lib/mdx'
+import { DinoCount } from '@/components/dinoCount'
 
-
-
-export default function IndexPage({
-  posts,
-  geoLocationData,
-  binData,
-}) {
-  const [count, setCount] = useState(binData.record.count)
-  const geo = geoLocationData
-  const [existingBinData, setExistingBinData] = useState(binData.record)
-  const isMounted= useRef(false)
-
-  function handleCountCountry() {
-    // if (count != 0) {
-    //   return
-    // }
-    setCount(count + 1)
-  }
-
-  useEffect(() => {
-    if (isMounted.current) {
-      let prevGeo = existingBinData.geoData
-    prevGeo.push(geo)
-    let newData = {
-      ...existingBinData,
-      count: count,
-      geoData: prevGeo,
-      
-    }
-
-    const requestOptions = {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(newData),
-    }
-    fetch(
-      'https://api.jsonbin.io/v3/b/62d97a21248d43754ffe585b',
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(data => console.log('Getting sneaky, huh?'))
-    }
-    else {
-      isMounted.current = true
-    }
-    
-  }, [count])
-
+export default function IndexPage({ posts }) {
   return (
     <Fragment>
       <SEO />
@@ -69,33 +23,7 @@ export default function IndexPage({
           <br />
           Also an undergrad engineering student in India.
         </p>
-
-        <div>
-          <div className="flex items-center justify-center w-full h-full">
-            <div className="w-32 h-32 overflow-hidden rounded-lg md:h-40 md:w-40 lg:h-48 lg:w-48">
-              <Image
-                src="/images/giphy.webp"
-                alt="My avatar"
-                width={256}
-                height={256}
-                quality={100}
-                priority={true}
-              />
-            </div>
-          </div>
-          <div className="mt-2 ml-2">
-            Here's someone greeting.{' '}
-            <span className="text-pink-600">
-              <b>Cute, right?</b>
-            </span>
-            <div
-              className="text-center mt-2 animate-bounce"
-              onClick={handleCountCountry}
-            >
-              💖 {count}
-            </div>
-          </div>
-        </div>
+        <DinoCount />
       </div>
       <Link href="/about">
         <p className="mt-5">
@@ -144,15 +72,7 @@ export const getStaticProps = async () => {
   const posts = await getAllFrontMatters()
   // const projects = await getProjects()
 
-  const resGeo = await fetch('https://geolocation-db.com/json/')
-  const geoLocationData = await resGeo.json()
-
-  const resBin = await fetch(
-    'https://api.jsonbin.io/v3/b/62d97a21248d43754ffe585b/latest'
-  )
-  const binData = await resBin.json()
-
   return {
-    props: { posts, geoLocationData, binData },
+    props: { posts },
   }
 }

77f42e79c364c63b5dd752c042db71b0d4cfddb8
Arpit Goyal <50814175+Arpitgoyalgg@users.noreply.github.com>
2022-09-20 17:13:13 +0530

    fix: favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="shortcut icon" href="/static/favicon.ico" type="icon" />
+          <link rel="shortcut icon" href="/static/giphy.png" type="icon" />
           <link
             rel="preload"
             href="/fonts/Inter.var.woff2"
